91271794763865fc2e82d7078cffa71f02056b1c
Max Tyler <maxastyler@gmail.com>
2019-06-17 14:10:54 +0100

    Add rusty counter binary crate

--- rusty-counter/src/main.rs
+++ rusty-counter/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+extern crate hhlib_sys;
+
+use hhlib_sys::device::Device;
+use hhlib_sys::types::HydraHarpError;
+
+fn main() -> Result<(), HydraHarpError> {
+    for i in 0..8 {
+        if let Ok(a) = Device::open_device(i) {
+            println!("{:?}", a.get_base_resolution());
+        }
+    }
+    Ok(())
 }
