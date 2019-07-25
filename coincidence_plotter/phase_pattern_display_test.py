b3a9ab1f57bc4c67d596526b8ed1d9df41f594ea
Max Tyler <maxastyler@gmail.com>
2019-07-25 09:39:24 +0100

    Add a lookup table control instead of a gradient editor

--- coincidence_plotter/phase_pattern_display_test.py
+++ coincidence_plotter/phase_pattern_display_test.py
@@ -23,29 +23,28 @@ class MainWindow(QWidget):
             (screens[1] if len(screens) > 1 else screens[0]))
         self.fs_plot.showFullScreen()
         self.little_plot = FullScreenPlot()
-        self.gradient_editor = pg.GradientWidget()
-        self.gradient_editor.loadPreset('grey')
-        self.gradient_editor.sigGradientChangeFinished.connect(
-            lambda s: self.fs_plot.update_LUT(s.getLookupTable(256)))
-        self.gradient_editor.sigGradientChangeFinished.connect(
-            lambda s: self.little_plot.update_LUT(s.getLookupTable(256)))
+        self.lut_control = LUTControls()
+        self.lut_control.valueChanged.connect(
+            lambda: self.fs_plot.update_LUT(self.lut_control.get_LUT()))
+        self.lut_control.valueChanged.connect(
+            lambda: self.little_plot.update_LUT(self.lut_control.get_LUT()))
         self.zernike_set = ZernikeSet()
         self.zernike_set.valueChanged.connect(self.set_new_image)
         layout.addWidget(self.phase_controller)
         layout.addWidget(self.little_plot)
-        layout.addWidget(self.gradient_editor)
+        layout.addWidget(self.lut_control)
         layout.addWidget(QLabel("Zernike values"))
         layout.addWidget(self.zernike_set)
         self.reset_button = QPushButton("Reset zernike values")
+        self.reset_button.clicked.connect(self.zernike_set.reset_values)
         layout.addWidget(self.reset_button)
 
     @pyqtSlot()
     def set_new_image(self):
         im = np.angle(
             combine_patterns_no_angle(X, Y,
-                                      [self.phase_controller.get_values()])
-            * self.zernike_set.get_values()
-        )
+                                      [self.phase_controller.get_values()]) *
+            self.zernike_set.get_values())
         self.fs_plot.set_image(im)
         self.little_plot.set_image(im)
 
@@ -54,6 +53,46 @@ class MainWindow(QWidget):
             self.fs_plot.close()
         except:
             pass
+
+
+class LUTControls(QWidget):
+    valueChanged = pyqtSignal()
+
+    def __init__(self):
+        super().__init__()
+        self.layout = QHBoxLayout()
+        self.setLayout(self.layout)
+        self.max_colour = pg.SpinBox(value=255,
+                                     int=True,
+                                     step=1,
+                                     bounds=(0, 255))
+        self.min_clamp = pg.SpinBox(value=-np.pi, bounds=(-np.pi, np.pi))
+        self.max_clamp = pg.SpinBox(value=np.pi, bounds=(-np.pi, np.pi))
+        self.layout.addWidget(QLabel("Max colour: "))
+        self.layout.addWidget(self.max_colour)
+        self.layout.addWidget(QLabel("Min Clamp: "))
+        self.layout.addWidget(self.min_clamp)
+        self.layout.addWidget(QLabel("Max Clamp: "))
+        self.layout.addWidget(self.max_clamp)
+        self.max_colour.sigValueChanged.connect(self.valueChanged.emit)
+        self.min_clamp.sigValueChanged.connect(self.valueChanged.emit)
+        self.max_clamp.sigValueChanged.connect(self.valueChanged.emit)
+
+    def get_LUT(self):
+        min_index = np.int(
+            np.round((self.min_clamp.value() + np.pi) / (2 * np.pi) * 255))
+        max_index = np.int(
+            np.round((self.max_clamp.value() + np.pi) / (2 * np.pi) * 255))
+        lut = np.empty((255, 3), dtype=np.uint8)
+        lut[:min_index] = 0
+        lut[max_index:] = self.max_colour.value()
+        if min_index < max_index:
+            lut[min_index:max_index, :] = np.tile(
+                np.linspace(0,
+                            self.max_colour.value(),
+                            abs(max_index - min_index),
+                            dtype=np.uint8), (3, 1)).T
+        return lut
 
 
 class ZernikeControl(QWidget):
@@ -80,9 +119,7 @@ class ZernikeSet(QWidget):
         self.setLayout(self.layout)
         indices = [(0, 0), (-1, 1), (1, 1), (-2, 2), (0, 2), (2, 2), (-3, 3),
                    (-1, 3), (1, 3), (3, 3)]
-        self.controls = [
-            ZernikeControl(i, 1 if i == (0, 0) else 0) for i in indices
-        ]
+        self.controls = [ZernikeControl(i, 0) for i in indices]
         self.value_dict = {}
         for c in self.controls:
             self.value_dict[c.index] = zernike_cartesian(*c.index)(X, Y)
@@ -93,6 +130,10 @@ class ZernikeSet(QWidget):
         return np.exp(1j * np.sum(
             [c.sbox.value() * self.value_dict[c.index] for c in self.controls],
             axis=0))
+
+    def reset_values(self):
+        for c in self.controls:
+            c.sbox.setValue(0)
 
 
 class FullScreenPlot(pg.PlotWidget):
