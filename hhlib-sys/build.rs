f690064c445faecef5026dfa73f750e79596498d
Max Tyler <maxastyler@gmail.com>
2019-08-07 09:51:35 +0100

    Add header includes for windows

--- hhlib-sys/build.rs
+++ hhlib-sys/build.rs
@@ -6,9 +6,9 @@ use std::path::{Path, PathBuf};
 fn main() {
 
     let target = env::var("TARGET").unwrap();
-
+    let mut lib_dir = Path::new("/");
     if target.contains("windows") {
-        let lib_dir = Path::new("C:\\Program Files\\PicoQuant\\HydraHarp-HHLibv30");
+        lib_dir = Path::new("C:\\Program Files\\PicoQuant\\HydraHarp-HHLibv30");
         println!("cargo:rustc-link-search={}", &lib_dir.display());
         println!("cargo:rustc-link-lib=hhlib");
     } else {
@@ -26,9 +26,16 @@ fn main() {
             .write_to_file(out_path.join("bindings.rs"))
             .expect("Couldn't write bindings!");
     } else {
-        let bindings = bindings_partial
+        let bindings = if target.contains("windows") {
+            bindings_partial
+            .clang_args(&[format!("-I {}", &lib_dir.display())])
             .generate()
-            .expect("Unable to generate bindings");
+            .expect("Unable to generate bindings")
+        } else {
+            bindings_partial
+            .generate()
+            .expect("Unable to generate bindings")
+        };
 
         let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
         bindings
